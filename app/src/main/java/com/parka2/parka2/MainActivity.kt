915e1a89fa1a2e5b012053cc62d39399eb502e0a
alexjalk <alexjalk@umich.edu>
2019-07-09 18:34:09 -0400

    Inital Commit

---
+++ app/src/main/java/com/parka2/parka2/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.parka2.parka2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        item->
+        when(item.itemId) {
+            R.id.map -> {
+                println("map pressed")
+                replaceFragment(MapFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.options -> {
+                println("options pressed")
+                replaceFragment(OptionsFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        replaceFragment(MapFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+}
