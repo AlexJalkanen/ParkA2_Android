20b0c466dcb6c97ac7db092fa4c5bfbeeb422d49
alexjalk <alexjalk@umich.edu>
2019-07-09 21:28:47 -0400

    Some progress

--- app/src/main/java/com/parka2/parka2/MainActivity.kt
+++ app/src/main/java/com/parka2/parka2/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.parka2.parka2
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v4.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
